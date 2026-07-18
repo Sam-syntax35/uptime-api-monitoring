4b05e16e3d265864af439397be3b71d246e0be58
Sam-syntax35 <mansisharma317305@gmail.com>
2026-07-18 20:32:48 +0530

    fix: log to console in production

--- server/src/shared/config/logger.js
+++ server/src/shared/config/logger.js
@@ -1,34 +1,32 @@
 import winston from "winston";
-import config from "./index.js"
+import config from "./index.js";
 
-/**
- * Winston logger configuration
- * Provides logging!
- */
-const logger = winston.createLogger({
-    level: config.node_env === "production" ? 'info' : 'debug',
-    format: winston.format.combine(
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.errors({ stack: true }),
-        winston.format.splat(),
-        winston.format.json()
-    ),
-
-    defaultMeta: { service: "api-monitoring" },
-
-    transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: "error" }),
-        new winston.transports.File({ filename: 'logs/combined.log' }),
-    ],
-})
+const transports = [
+  new winston.transports.Console(), // Always log to Render console
+];
 
 if (config.node_env !== "production") {
-    logger.add(new winston.transports.Console({
-        format: winston.format.combine(
-            winston.format.colorize(),
-            winston.format.simple()
-        )
-    }))
+  transports.push(
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+    }),
+    new winston.transports.File({
+      filename: "logs/combined.log",
+    })
+  );
 }
 
-export default logger
+const logger = winston.createLogger({
+  level: config.node_env === "production" ? "info" : "debug",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "api-monitoring" },
+  transports,
+});
+
+export default logger;
